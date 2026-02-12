abd0373d51831aa08119caa2858b50779156315a
yogeswar142 <amarayogeswararao16@gmail.com>
2026-02-12 12:23:09 +0530

    Delete app/src/main directory

--- app/src/main/java/com/ghostmode/app/core/notification/MessageNotificationListenerService.kt
+++
@@ -1,6 +0,0 @@
-package com.ghostmode.app.core.notification
-
-import android.service.notification.NotificationListenerService
-
-class MessageNotificationListenerService : NotificationListenerService()
-
--- app/src/main/java/com/ghostmode/app/data/local/chat/dao/ChatMessageDao.kt
+++
@@ -1,7 +0,0 @@
-package com.ghostmode.app.data.local.chat.dao
-
-import androidx.room.Dao
-
-@Dao
-interface ChatMessageDao
-
